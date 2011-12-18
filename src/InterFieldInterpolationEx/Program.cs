45ee58196ad36634edcc2c8ff823c6aa297a2683
rivergod <rivernile@naver.com>
2011-12-18 10:47:21 +0900

    Impl InterFieldInterpolationEx

--- src/InterFieldInterpolationEx/Program.cs
+++ src/InterFieldInterpolationEx/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -10,21 +11,61 @@ namespace InterFieldInterpolationEx
 	{
 		static void Main(string[] args)
 		{
-			if (args.Length < 1 || !(File.Exists(Path.GetFullPath(args[0]))))
+			if (args.Length < 2 || !(File.Exists(Path.GetFullPath(args[0]))))
 			{
 				Help();
 				Environment.Exit(0);
 			}
 
+			List<string> tmpImgFiles = new List<string>(File.ReadAllLines(Path.GetFullPath(args[0])));
 			List<string> imgFiles = new List<string>();
 
+			tmpImgFiles.ForEach(txt => imgFiles.Add(Path.GetFullPath(txt)));
 
+			//For List Debug
+			//imgFiles.ForEach(txt => System.Diagnostics.Trace.WriteLine(txt));
+
+			int currPos = 0;
+
+			// 버퍼의 처음 이미지를 초기화 하고 블랙으로 채운다.
+			Bitmap currBitmap = new Bitmap(720, 480);
+			for (int j = 0; j < currBitmap.Height; j++)
+			{
+				for (int i = 0; i < currBitmap.Width; i++)
+				{
+					currBitmap.SetPixel(i, j, Color.Black);
+				}
+			}
+
+			imgFiles.ForEach(path => { 
+				Program.DoStep(ref currBitmap, ref currPos, path);
+				currBitmap.Save(Path.GetFullPath(args[1] + String.Format("{0:D6}", currPos)) + ".bmp");
+				System.Console.Write(".");
+			});
+			System.Console.Write("\n");
+		}
+
+		public static void DoStep(ref Bitmap prevBitmap, ref int currPos, string inputPath)
+		{
+			currPos++;
+
+			Boolean isodd = (currPos % 2 == 0);
+
+			Bitmap inputBitmap = new Bitmap(inputPath);
+
+			for (int j = (isodd ? 0 : 1 ) ; j < prevBitmap.Height; j += 2)
+			{
+				for (int i = 0; i < prevBitmap.Width; i++)
+				{
+					prevBitmap.SetPixel(i, j, inputBitmap.GetPixel(i, (int)(j / 2)));
+				}
+			}
 		}
 
 		public static void Help()
 		{
 			Console.WriteLine("InterFieldInterpolationEx");
-			Console.WriteLine(" Usage: Cammand ImageListFileName");
+			Console.WriteLine(" Usage: Cammand ImageListFileName outputFilePreName");
 		}
 	}
 }
